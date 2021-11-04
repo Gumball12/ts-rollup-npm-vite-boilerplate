9c516ec80ae3e379979248e23b65d1b55efc8d9c
Gumball12 <to@shj.rip>
2021-11-05 02:09:48 +0900

    feat: add sandbox

---
+++ sandbox/main.js
@@ -0,0 +1,20 @@
+import { html, render } from 'htm/preact';
+import { useState } from 'preact/hooks';
+import { hello } from '../dist/index.js';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  setInterval(setCount, 1000, count + 1);
+
+  return html`
+    <main>
+      <p>
+        ${hello} from boilerplate!<br />
+        count: ${count}
+      </p>
+    </main>
+  `;
+};
+
+render(html`<${App} />`, document.body);
